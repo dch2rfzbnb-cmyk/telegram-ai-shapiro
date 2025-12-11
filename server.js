54bbce95afff78f0535da8dd94e901ef5ce0063d
dch2rfzbnb-cmyk <dch2rfzbnb@privaterelay.appleid.com>
2025-12-11 03:20:30 +0300

    Fix deploy simple version

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Главный маршрут — отдаем index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index-simple.html'));  // ← ИЗМЕНИ!
 });
 
 // Запуск сервера
